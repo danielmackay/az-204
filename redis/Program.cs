e1ccc5e71385fe2c4bcebf3db04d6d54a18080b0
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2023-03-07 21:08:57 +1000

    Added redis

---
+++ redis/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using StackExchange.Redis;
+
+var host = Host.CreateDefaultBuilder().Build();
+
+host.Start();
+
+var config = host.Services.GetRequiredService<IConfiguration>();
+var connectionString = config.GetConnectionString("Redis");
+ArgumentNullException.ThrowIfNull(connectionString);
+
+using var cache = ConnectionMultiplexer.Connect(connectionString);
+
+IDatabase db = cache.GetDatabase();
+
+// Snippet below executes a PING to test the server connection
+var result = await db.ExecuteAsync("ping");
+Console.WriteLine($"PING = {result.Type} : {result}");
+
+// Call StringSetAsync on the IDatabase object to set the key "test:key" to the value "100"
+var setValue = await db.StringSetAsync("test:key", "100");
+Console.WriteLine($"SET: {setValue}");
+
+// StringGetAsync takes the key to retrieve and return the value
+var getValue = await db.StringGetAsync("test:key");
+Console.WriteLine($"GET: {getValue}");
