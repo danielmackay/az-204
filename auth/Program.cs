423250857dda1e244ff8abfb8052fdda0eee35fb
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2023-03-18 20:27:15 +1000

    Added MSAL application

---
+++ auth/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Identity.Client;
+
+var host = Host
+    .CreateDefaultBuilder(args)
+    .Build();
+
+host.Start();
+
+var config = host.Services.GetRequiredService<IConfiguration>();
+
+string? _clientId = config.GetValue<string>("clientId");
+string? _tenantId = config.GetValue<string>("tenantId");
+
+ArgumentNullException.ThrowIfNullOrEmpty(_clientId);
+ArgumentNullException.ThrowIfNullOrEmpty(_tenantId);
+
+var app = PublicClientApplicationBuilder
+    .Create(_clientId)
+    .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
+    .WithRedirectUri("http://localhost")
+    .Build();
+
+string[] scopes = { "user.read" };
+
+AuthenticationResult result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+
+Console.WriteLine($"Token:\t{result.AccessToken}");
+
+Console.WriteLine("Press any key to exit");
